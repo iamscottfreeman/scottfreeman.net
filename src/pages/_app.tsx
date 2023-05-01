ac8fb2172c3cf974b13e30e96fb978a9b4a128d0
Scott Freeman <git@scottfreeman.net>
2023-05-01 17:50:04 +0100

    Added SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import '@/styles/globals.css';
+import { NextSeo } from 'next-seo';
 import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextSeo
+        title='IPL Tested World Championships'
+        description='IPL Tested World Championships in Coventry, UK on 18th to 22nd October, 2023.'
+      />
       <Component {...pageProps} />
       <Analytics />
     </>

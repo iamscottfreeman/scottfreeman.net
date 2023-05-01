e71f83b95a6fd7082a8972bb0338aec261f94783
Scott Freeman <git@scottfreeman.net>
2023-05-01 17:04:06 +0100

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className='bg-black text-white flex min-h-screen items-center justify-center align-middle'>
+      <div className='text-5xl'>scottfreeman.net</div>
+    </main>
+  );
+}
